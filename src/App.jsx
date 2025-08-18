aa895b26b6ff99866246180a474fd6b27dd03e04
MohaX455 <mohamedhtk620@gmail.com>
2025-08-18 09:18:57 +0100

    sauvegarde avant rebase

--- src/App.jsx
+++ src/App.jsx
@@ -8,43 +8,44 @@ import Contact from './components/Contact.jsx'
 import Footer from './components/Footer.jsx'
 
 const App = () => {
-  const [success, setSuccess] = useState(false)
+    const [success, setSuccess] = useState(false)
 
-  // Disparition auto du popup après 3s
-  useEffect(() => {
-    if (success) {
-      const timer = setTimeout(() => setSuccess(false), 3000)
-      return () => clearTimeout(timer)
-    }
-  }, [success])
+    // Disparition auto du popup après 3s
+    useEffect(() => {
+        if (success) {
+            const timer = setTimeout(() => setSuccess(false), 3000)
+            return () => clearTimeout(timer)
+        }
+    }, [success])
 
-  return (
-    <div className="font-[poppins] text-gray-700 relative tracking-wide transition duration-300 scroll-smooth">
+    return (
+        <div className="font-['Nata Sans'] text-gray-700 relative tracking-wide transition duration-300 scroll-smooth">
 
-      {/* ✅ Popup avec animation descendante */}
-      <div className={`
+            {/* ✅ Popup avec animation descendante */}
+            <div className={`
         fixed top-5 left-1/2 transform -translate-x-1/2 z-60 
         transition-all duration-500 ease-in-out
         ${success ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0 pointer-events-none'}
       `}>
-        <div className="bg-[#222] w-[70%] md:w-fit h-[90px] px-6 flex justify-center items-center 
-                        border-2 border-[rgba(255_255_255_/_0.3)] rounded-md shadow-2xl">
-          <p className="text-white font-medium text-[15px] md:text-[17px]">
-            ✅ Message sent successfully!
-          </p>
-        </div>
-      </div>
+                <div className="bg-[#222] w-fit h-[90px] px-6 flex justify-center items-center 
+                        border-[1.5px] border-[rgba(255_255_255_/_0.3)] rounded-lg shadow-2xl">
+                    <p className="text-white text-center font-medium text-[14px] md:text-[17px] flex items-center justify-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="text-green-400 lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5" /></svg>
+                        Message sent successfully!
+                    </p>
+                </div>
+            </div>
 
-      {/* Composants */}
-      <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact setSuccess={setSuccess} />
-      <Footer />
-    </div>
-  )
+            {/* Composants */}
+            <Navbar />
+            <Hero />
+            <About />
+            <Skills />
+            <Projects />
+            <Contact setSuccess={setSuccess} />
+            <Footer />
+        </div>
+    )
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,38 +61,54 @@ const Navbar = () => {
 
             {/* Mobil Menue */}
 
-            <div className={`fixed inset-0 bg-white z-50 flex flex-col justify-center items-center transition-all duration-300 ${showMobileMenu ? 'translate-x-0' : 'translate-x-full'} md:hidden`}>
-                <div className='flex justify-between md:justify-end items-center absolute md:relative top-[20px] md:top-0 left-0 w-full h-[60px] md:h-0 px-8 md:px-0'>
+            <div className={`fixed inset-0 bg-white z-50 flex flex-col transition-all duration-300 ${showMobileMenu ? 'translate-x-0' : 'translate-x-full'} md:hidden`}>
+                <div className='bg-green flex justify-between md:justify-end items-center sticky md:relative top-[20px] md:top-0 left-0 w-full h-[100px] md:h-0 px-8 md:px-0'>
                     <div className='flex md:hidden'>
-                        <a href="#" className="text-3xl font-medium text-orange-500 cursor-pointer">MohaX</a>
+                        <a href="#" className="text-[22px] font-medium text-orange-500 cursor-pointer">MohaX</a>
                     </div>
                     {/* <span className='absolute top-10 right-8 md:top-8 md:right-8 cursor-pointer text-[18px] text-[#888]'> */}
                     <span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            className="stroke-3 size-[25px] md:size-[23px] cursor-pointer text-[#999] lucide lucide-x-icon lucide-x" onClick={handleCloseMobileMenu}>
+                            className="stroke-2 size-[23px] cursor-pointer text-[#999] lucide lucide-x-icon lucide-x" onClick={handleCloseMobileMenu}>
                             <path d="M18 6 6 18" />
                             <path d="m6 6 12 12" />
                         </svg>
                     </span>
                 </div>
 
-                <nav className='flex flex-col w-full justify-between gap-8'>
-                    <div className="flex flex-col items-center text-center gap-8 text-[20px]">
-                        <a href="#" className='transition duration-300 hover:text-orange-500' onClick={handleCloseMobileMenu}>Home</a>
-                        <a href="#About" className='transition duration-300 hover:text-orange-500' onClick={handleCloseMobileMenu}>About</a>
-                        <a href="#Skills" className='transition duration-300 hover:text-orange-500' onClick={handleCloseMobileMenu}>Skills</a>
-                        <a href="#Projects" className='transition duration-300 hover:text-orange-500' onClick={handleCloseMobileMenu}>Project</a>
+                <nav className='flex flex-col w-full justify-between gap-8 h-fit'>
+                    <div className="h-[1px] w-[90%] m-auto my-8 bg-[rgba(0_0_0_/_0.3)]"></div>
+                    <div className="flex flex-col gap-8 text-[16px] pl-15">
+                        <a href="#" className='flex items-center gap-2 transition duration-300 hover:text-orange-500' onClick={handleCloseMobileMenu}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house-icon lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" /><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /></svg>
+                            Home
+                        </a>
+                        <a href="#About" className='flex items-center gap-2 transition duration-300 hover:text-orange-500' onClick={handleCloseMobileMenu}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" /><circle cx="12" cy="7" r="4" /></svg>
+                            About
+                        </a>
+                        <a href="#Skills" className='flex items-center gap-2 transition duration-300 hover:text-orange-500' onClick={handleCloseMobileMenu}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cpu-icon lucide-cpu"><path d="M12 20v2" /><path d="M12 2v2" /><path d="M17 20v2" /><path d="M17 2v2" /><path d="M2 12h2" /><path d="M2 17h2" /><path d="M2 7h2" /><path d="M20 12h2" /><path d="M20 17h2" /><path d="M20 7h2" /><path d="M7 20v2" /><path d="M7 2v2" /><rect x="4" y="4" width="16" height="16" rx="2" /><rect x="8" y="8" width="8" height="8" rx="1" /></svg>
+                            Skills
+                        </a>
+                        <a href="#Projects" className='flex items-center gap-2 transition duration-300 hover:text-orange-500' onClick={handleCloseMobileMenu}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder-open-icon lucide-folder-open"><path d="m6 14 1.5-2.9A2 2 0 0 1 9.24 10H20a2 2 0 0 1 1.94 2.5l-1.54 6a2 2 0 0 1-1.95 1.5H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h3.9a2 2 0 0 1 1.69.9l.81 1.2a2 2 0 0 0 1.67.9H18a2 2 0 0 1 2 2v2" /></svg>
+                            Project
+                        </a>
                     </div>
-                    <div className="h-[1px] w-[70%] m-auto my-8 bg-[rgba(0_0_0_/_0.3)]"></div>
+                    <div className="h-[1px] w-[90%] m-auto my-8 bg-[rgba(0_0_0_/_0.3)]"></div>
                     <div className='mx-auto'>
                         <a href="#Contact"
-                            className="px-14 py-3.5 bg-white transition duration-300 text-orange-500 text-[18px] font-medium rounded-full border border-orange-500 cursor-pointer" onClick={handleCloseMobileMenu}>Contact me</a>
+                            className="flex flex-row-reverse gap-2 justify-center items-center px-16 py-3.5 bg-white transition-all duration-500 text-orange-500 text-[16px] font-medium rounded-md hover:rounded-full border border-orange-500 cursor-pointer" onClick={handleCloseMobileMenu}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rocket-icon lucide-rocket"><path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z" /><path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" /><path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" /><path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" /></svg>
+                            Contact me
+                        </a>
                     </div>
 
-                    <div className='absolute bottom-[15px] left-1/2 -translate-1/2 max-w-3xl'>
+                    {/* <div className='absolute bottom-[15px] left-1/2 -translate-1/2 max-w-3xl'>
                         <p className="text-[14.5px] md:text-[13px] text-center text-[rgba(0_0_0_/_0.5)]">&copy; 2025 MohaX. All rights reserved</p>
-                    </div>
+                    </div> */}
 
                 </nav>
 
